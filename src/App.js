98909e4e746cff36dd9f6780ee37dc9831167355
tractortoby <tobychung1993@gmail.com>
2021-11-29 22:54:58 +0800

    Add styles.css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React,{useReducer, useRef} from 'react'
+import React from "react";
+import "./styles.css";
+const { useReducer, useRef } = React;
 
 const initialState = {
   additionalPrice: 0,
   car: {
     price: 26395,
     name: "2019 Ford Mustang",
-    image: "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
-    features: []
+    image:
+      "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
+    features: [],
   },
   store: [
     { id: 1, name: "V-6 engine", price: 1500 },
     { id: 2, name: "Racing detail package", price: 1500 },
     { id: 3, name: "Premium sound system", price: 500 },
-    { id: 4, name: "Rear spoiler", price: 250 }
-  ]
+    { id: 4, name: "Rear spoiler", price: 250 },
+  ],
 };
 
 const reducer = (state, action) => {
@@ -22,33 +25,36 @@ const reducer = (state, action) => {
       return {
         ...state,
         additionalPrice: state.additionalPrice - action.item.price,
-        car: { ...state.car, features: state.car.features.filter((x) => x.id !== action.item.id)},
-        store: [...state.store, action.item]
+        car: {
+          ...state.car,
+          features: state.car.features.filter((x) => x.id !== action.item.id),
+        },
+        store: [...state.store, action.item],
       };
     case "BUY_ITEM":
       return {
         ...state,
         additionalPrice: state.additionalPrice + action.item.price,
         car: { ...state.car, features: [...state.car.features, action.item] },
-        store: state.store.filter((x) => x.id !== action.item.id)
-      }
+        store: state.store.filter((x) => x.id !== action.item.id),
+      };
     default:
       return state;
   }
-}
+};
 
 const App = () => {
   const inputRef = useRef();
   const [state, dispatch] = useReducer(reducer, initialState);
-  
+
   const removeFeature = (item) => {
-    dispatch({ type: 'REMOVE_ITEM', item });
-  }
-  
+    dispatch({ type: "REMOVE_ITEM", item });
+  };
+
   const buyItem = (item) => {
-    dispatch({ type: 'BUY_ITEM', item })
-  }
-  
+    dispatch({ type: "BUY_ITEM", item });
+  };
+
   return (
     <div className="boxes">
       <div className="box">
@@ -59,50 +65,50 @@ const App = () => {
         <p>Amount: ${state.car.price}</p>
         <div className="content">
           <h6>Added features:</h6>
-          {state.car.features.length ? 
-            (
-              <ol type="1">
-                {state.car.features.map((item) => (
-                  <li key={item.id}>
-                    <button
-                      onClick={() => removeFeature(item)}
-                      className="button">X
-                    </button>
-                    {item.name}
-                  </li>
-                ))}
-              </ol>
-            ) : <p>You can purchase items from the store.</p>
-          }
+          {state.car.features.length ? (
+            <ol type="1">
+              {state.car.features.map((item) => (
+                <li key={item.id}>
+                  <button
+                    onClick={() => removeFeature(item)}
+                    className="button"
+                  >
+                    X
+                  </button>
+                  {item.name}
+                </li>
+              ))}
+            </ol>
+          ) : (
+            <p>You can purchase items from the store.</p>
+          )}
         </div>
       </div>
       <div className="box">
         <div className="content">
           <h4>Additional Features</h4>
-          {state.store.length ? 
-            (
+          {state.store.length ? (
             <ol type="1">
               {state.store.map((item) => (
                 <li key={item.id}>
-                  <button
-                    onClick={() => buyItem(item)}
-                    className="button">Add</button>
+                  <button onClick={() => buyItem(item)} className="button">
+                    Add
+                  </button>
                   {item.name} (+{item.price})
                 </li>
               ))}
             </ol>
-            ) : <p>Nice looking car!</p>
-          }
+          ) : (
+            <p>Nice looking car!</p>
+          )}
         </div>
 
         <div className="content">
-        <h4>
-          Total Amount: ${state.car.price + state.additionalPrice}
-        </h4>
-      </div>
+          <h4>Total Amount: ${state.car.price + state.additionalPrice}</h4>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default App
+export default App;
